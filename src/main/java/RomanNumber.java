a07cd9b1de7f8534bdabfcd1c130f220119571b9
dmitrykos <dmitrykos@neutroncode.com>
2020-02-24 15:07:43 +0200

    applied code reformatting, cleanup

--- src/main/java/RomanNumber.java
+++ src/main/java/RomanNumber.java
@@ -1,9 +1,7 @@
-public class RomanNumber
-{
+public class RomanNumber {
     int remainder;
 
-    public String calculate(int decimalNumber)
-    {
+    public String calculate(int decimalNumber) {
         remainder = decimalNumber;
 
         String ret = "";
@@ -15,16 +13,13 @@ public class RomanNumber
         return ret;
     }
 
-    private String getLiteralC(int decimalNumber)
-    {
+    private String getLiteralC(int decimalNumber) {
         int divisionValue = decimalNumber / 100;
-
         String ret = "";
-        if(decimalNumber >= 90 && divisionValue == 0){
+        if (decimalNumber >= 90 && divisionValue == 0) {
             ret = "XC";
             remainder -= 90;
-        }
-        else {
+        } else {
             for (int i = 0; i < divisionValue; i++) {
                 ret += "C";
             }
@@ -32,16 +27,14 @@ public class RomanNumber
         }
         return ret;
     }
-    private String getLiteralL(int decimalNumber)
-    {
-        int divisionValue = decimalNumber / 50;
 
+    private String getLiteralL(int decimalNumber) {
+        int divisionValue = decimalNumber / 50;
         String ret = "";
-        if(decimalNumber >= 40 && divisionValue == 0){
+        if (decimalNumber >= 40 && divisionValue == 0) {
             ret = "XL";
             remainder -= 40;
-        }
-        else {
+        } else {
             for (int i = 0; i < divisionValue; i++) {
                 ret += "L";
             }
@@ -49,48 +42,41 @@ public class RomanNumber
         }
         return ret;
     }
-    private String getLiteralX(int decimalNumber)
-    {
-        int divisionValue = decimalNumber / 10;
 
+    private String getLiteralX(int decimalNumber) {
+        int divisionValue = decimalNumber / 10;
         String ret = "";
-        if(decimalNumber ==9){
+        if (decimalNumber == 9 && divisionValue == 0) {
             ret = "IX";
             remainder = 0;
-        }
-        else {
+        } else {
             for (int i = 0; i < divisionValue; i++) {
                 ret += "X";
             }
+            remainder -= divisionValue * 10;
         }
-        remainder -= divisionValue * 10;
         return ret;
     }
-    private String getLiteralV(int decimalNumber)
-    {
-        int divisionValue = decimalNumber / 5;
 
-        if (decimalNumber > 8)
-        {
+    private String getLiteralV(int decimalNumber) {
+        int divisionValue = decimalNumber / 5;
+        if (decimalNumber > 8) {
             remainder = 0;
             return "IX";
         }
-
         remainder -= divisionValue * 5;
         if (divisionValue != 0)
             return "V";
-
         return "";
     }
 
-    private String getLiteralI(int decimalNumber)
-    {
+    private String getLiteralI(int decimalNumber) {
         String ret = "";
-        if(decimalNumber == 4){
+        if (decimalNumber == 4) {
             ret = "IV";
             return ret;
         }
-        for(int i=0; i < decimalNumber;i++){
+        for (int i = 0; i < decimalNumber; i++) {
             ret += "I";
         }
         return ret;
--- src/test/java/RomanNumbersTest.java
+++ src/test/java/RomanNumbersTest.java
@@ -3,70 +3,77 @@ import org.junit.Test;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
-public class RomanNumbersTest
-{
+public class RomanNumbersTest {
     RomanNumber number = new RomanNumber();
 
     @Test
-    public void testPredefinedResult1To5()
-    {
+    public void testPredefinedResult1To5() {
         assertThat(number.calculate(1), equalTo("I"));
         assertThat(number.calculate(2), equalTo("II"));
         assertThat(number.calculate(3), equalTo("III"));
         assertThat(number.calculate(4), equalTo("IV"));
         assertThat(number.calculate(5), equalTo("V"));
     }
-    @Test public void testPredefinedResult6To10()
-    {
+
+    @Test
+    public void testPredefinedResult6To10() {
         assertThat(number.calculate(6), equalTo("VI"));
         assertThat(number.calculate(7), equalTo("VII"));
         assertThat(number.calculate(8), equalTo("VIII"));
         assertThat(number.calculate(9), equalTo("IX"));
         assertThat(number.calculate(10), equalTo("X"));
     }
-    @Test public void testPredefinedResult11To15()
-    {
+
+    @Test
+    public void testPredefinedResult11To15() {
         assertThat(number.calculate(11), equalTo("XI"));
         assertThat(number.calculate(12), equalTo("XII"));
         assertThat(number.calculate(14), equalTo("XIV"));
         assertThat(number.calculate(15), equalTo("XV"));
     }
-    @Test public void testPredefinedResult16To20()
-    {
+
+    @Test
+    public void testPredefinedResult16To20() {
         assertThat(number.calculate(16), equalTo("XVI"));
         assertThat(number.calculate(17), equalTo("XVII"));
         assertThat(number.calculate(19), equalTo("XIX"));
         assertThat(number.calculate(20), equalTo("XX"));
     }
-    @Test public void testPredefinedResult30To50()
-    {
+
+    @Test
+    public void testPredefinedResult30To50() {
         assertThat(number.calculate(38), equalTo("XXXVIII"));
         assertThat(number.calculate(40), equalTo("XL"));
         assertThat(number.calculate(42), equalTo("XLII"));
         assertThat(number.calculate(46), equalTo("XLVI"));
     }
-    @Test public void testPredefinedResult50s()
-    {
+
+    @Test
+    public void testPredefinedResult50s() {
         assertThat(number.calculate(50), equalTo("L"));
     }
-    @Test public void testPredefinedResult80s()
-    {
+
+    @Test
+    public void testPredefinedResult80s() {
         assertThat(number.calculate(80), equalTo("LXXX"));
         assertThat(number.calculate(86), equalTo("LXXXVI"));
     }
-    @Test public void testPredefinedResult90s()
-    {
+
+    @Test
+    public void testPredefinedResult90s() {
         assertThat(number.calculate(90), equalTo("XC"));
         assertThat(number.calculate(91), equalTo("XCI"));
         assertThat(number.calculate(98), equalTo("XCVIII"));
     }
-    @Test public void testPredefinedResult100s()
-    {
+
+    @Test
+    public void testPredefinedResult100s() {
         assertThat(number.calculate(100), equalTo("C"));
         assertThat(number.calculate(101), equalTo("CI"));
     }
-    @Test public void testPredefinedResult300s()
-    {
+
+    @Test
+    public void testPredefinedResult300s() {
         assertThat(number.calculate(387), equalTo("CCCLXXXVII"));
     }
 }
